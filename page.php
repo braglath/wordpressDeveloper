ae2816729804d0ff636cf4b62950b8b2102a010b
braglath <braglath44@gmail.com>
2021-08-19 21:38:18 +0530

    famous loops

--- page.php
+++ page.php
@@ -1,6 +1,7 @@
 <?php
 while(have_posts()){
     the_post(); ?>
+<h1>This is a page, not a post.</h1>
 <h1><?php the_title(); ?></h1>
 <h2 class="datetime"><?php the_date(); the_time(); ?></h2>
 <?php the_content(); ?>
